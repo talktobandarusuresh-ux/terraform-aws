e4af77dd59ea45baffde7afc3e5f5eff6db36064
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-12-27 13:53:31 +0530

    Terraform

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "example" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.Allow_all.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "Allow_all" {
+  name = "Allow_all"
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
